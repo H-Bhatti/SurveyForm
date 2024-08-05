0801111438f84518b8918ef651f626a394168188
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-08-05 20:30:48 +0400

    restructured the file system

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import { json } from "./components/surveyQuestions/surveyJson";
+
+function App() {
+  const survey = new Model(json);
+  survey.onComplete.add((sender) => {
+    console.log(sender.data);
+  });
+
+  return <Survey model={survey} />;
+}
+
+export default App;
