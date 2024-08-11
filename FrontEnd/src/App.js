41c891ee763f4bb9993463e7917ffbfe311c52b5
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-08-11 17:42:35 +0400

    data finally going to the backend

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,11 +3,20 @@ import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import { json } from "./components/surveyQuestions/surveyJson";
+import axios from "axios";
 
 function App() {
   const survey = new Model(json);
-  survey.onComplete.add((sender) => {
-    console.log(sender.data);
+  survey.onComplete.add(async (sender) => {
+    let msg = sender.data;
+    console.log(msg);
+    try {
+      await axios.post("http://localhost:5000/", {
+        msg,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   });
   return <Survey model={survey} />;
 }
