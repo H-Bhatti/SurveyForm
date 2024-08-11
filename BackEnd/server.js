0a6a67690a4554e895a5370dd9f2fc5e50ec0788
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-08-11 17:42:16 +0400

    data finally going to the backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
-//for parsing json data to server side
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-// POST /greet endpoint
-app.post("/greet", (req, res) => {});
+let port = parseInt(process.env.PORT);
+
+app.post("/", (req, res) => {
+  const { msg } = req.body;
+  const data = {
+    msg: msg,
+  };
+
+  console.log(data);
+});
 
 // Start the server
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server started  on port 5000");
 });
 //start to listen on specified port
