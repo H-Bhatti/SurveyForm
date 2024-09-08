4e5aadaa414d1124ec1a7603f7c68f03fc12dc1f
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-09-08 16:48:40 +0400

    made a function to send data

--- BackEnd/databaseConnections/database.js
+++ BackEnd/databaseConnections/database.js
@@ -4,34 +4,25 @@ const mysql = require("mysql2");
 require("dotenv").config();
 let HOST = process.env.HOST;
 let USER = process.env.USER;
-let DATABASE = process.env.PASSWORD;
-let PASSWORD = process.env.DATABASE;
+let DATABASE = process.env.DATABASE;
+let PASSWORD = process.env.PASSWORD;
 
 // Create the connection to database
 const pool = mysql.createConnection({
-  host: HOST,
-  user: USER,
-  password: DATABASE,
-  database: PASSWORD,
+  host: "127.0.0.1",
+  user: "root",
+  password: "password",
+  database: "mydb",
 });
 
 // create connection to database
 
-// sending to database
-const sql =
-  "INSERT INTO `testdata`(`Q1`,`Q2`,`Q3`,`Q4`,`Q5`,`Q6`,`Q7`,`Q8`,`Q9`) VALUES (?,?,?,?,?,?,?,?,?)";
-const values = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-pool.execute(
-  {
-    sql,
-    values,
-  },
-  (err, result, fields) => {
-    if (err instanceof Error) {
-      console.log(err);
-      return;
-    }
-    console.log(result);
-    console.log(fields);
-  }
-);
+function sendData(dataToSend) {
+  const result = pool.query(
+    `INSERT INTO testdata (Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9) VALUES (?,?,?,?,?,?,?,?,?)`,
+    dataToSend
+  );
+  return result;
+}
+
+console.log(sendData([1, 1, 1, 1, 1, 1, 1, 5, 5]));
